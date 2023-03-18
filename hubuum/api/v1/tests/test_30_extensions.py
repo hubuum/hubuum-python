67d9f06ccdc7c65444f0c5bd631a3d345387f700
Terje Kvernes <terje@kvernes.no>
2023-03-18 23:42:11 +0100

    Test and URL cleanups.
      - Unused names removed from URLs.

--- hubuum/api/v1/tests/test_30_extensions.py
+++ hubuum/api/v1/tests/test_30_extensions.py
@@ -1,94 +1,80 @@
 """Test hubuum extensions."""
+from hubuum.models import Host, Namespace
+
 from .base import HubuumAPITestCase
 
 
-class APIExtension(HubuumAPITestCase):
+class HubuumExtensionTestCase(HubuumAPITestCase):
+    """Base class for testing Hubuum Extensions."""
+
+    def setUp(self):
+        """Set up the test environment for the class."""
+        self.client = self.get_superuser_client()
+        self.namespace, _ = Namespace.objects.get_or_create(name="test")
+        self.extension_blob = {
+            "namespace": self.namespace.id,
+            "name": "fleet",
+            "model": "host",
+            "url": "https://fleet.my.domain/api/v1/fleet/hosts/identifier/{fqdn}",
+            "header": "Authorization: Bearer sh...==",
+        }
+        self.host, _ = Host.objects.get_or_create(name="test", namespace=self.namespace)
+
+    def tearDown(self) -> None:
+        """Tear down the test environment for the class."""
+        self.namespace.delete()
+        return super().tearDown()
+
+    def _extension_data_blob(self, extension_id, value="value", content_type="host"):
+        """Create an extension data blob."""
+        return {
+            "namespace": self.namespace.id,
+            "extension": extension_id,
+            "content_type": content_type,
+            "object_id": self.host.id,
+            "json_data": {"key": value, "listkey": [1, 2, 3]},
+        }
+
+
+class APIExtension(HubuumExtensionTestCase):
     """Test extensions."""
 
     def test_creating_extension(self):
         """Create extension."""
-        self.get_superuser_client()
-        nsblob = self.assert_post("/namespaces/", {"name": "test"})
-        exblob = self.assert_post(
-            "/extensions/",
-            {
-                "namespace": nsblob.data["id"],
-                "name": "fleet",
-                "model": "host",
-                "url": "https://fleet.my.domain/api/v1/fleet/hosts/identifier/{fqdn}",
-                "header": "Authorization: Bearer sh...==",
-            },
-        )
+        exblob = self.assert_post("/extensions/", self.extension_blob)
         exid = exblob.data["id"]
         self.assert_get_elements("/extensions/", 1)
         self.assert_get(f"/extensions/{exid}")
         self.assert_get("/extensions/fleet")
         self.assert_delete("/extensions/fleet")
-        self.assert_delete("/namespaces/test")
 
 
-class APIExtensionsData(HubuumAPITestCase):
+class APIExtensionsData(HubuumExtensionTestCase):
     """Test extension data."""
 
     def test_create_and_update_extension_data(self):
         """Create and update extension data."""
-        self.get_superuser_client()
-        nsblob = self.assert_post("/namespaces/", {"name": "test"})
-        hostblob = self.assert_post(
-            "/hosts/", {"name": "test", "namespace": nsblob.data["id"]}
-        )
-
-        exblob = self.assert_post(
-            "/extensions/",
-            {
-                "namespace": nsblob.data["id"],
-                "name": "fleet",
-                "model": "host",
-                "url": "https://fleet.my.domain/api/v1/fleet/hosts/identifier/{fqdn}",
-                "header": "Authorization: Bearer sh...==",
-            },
-        )
+        exblob = self.assert_post("/extensions/", self.extension_blob)
+        extension_id = exblob.data["id"]
 
         exdblob = self.assert_post(
-            "/extension_data/",
-            {
-                "namespace": nsblob.data["id"],
-                "extension": exblob.data["id"],
-                "content_type": "host",
-                "object_id": hostblob.data["id"],
-                "json_data": {"key": "value", "listkey": [1, 2, 3]},
-            },
+            "/extension_data/", self._extension_data_blob(extension_id)
         )
-
         exdid = exdblob.data["id"]
         self.assert_get(f"/extension_data/{exdid}")
-
         self.assertTrue(exdblob.data["json_data"]["key"] == "value")
 
         # Updating by using post
-        exdblob = self.assert_post(
+        newvalue = "newvalue"
+        self.assert_post(
             "/extension_data/",
-            {
-                "namespace": nsblob.data["id"],
-                "extension": exblob.data["id"],
-                "content_type": "host",
-                "object_id": hostblob.data["id"],
-                "json_data": {"key": "newvalue", "listkey": [1, 2, 3]},
-            },
+            self._extension_data_blob(extension_id, value=newvalue),
         )
-
-        self.assertTrue(exdblob.data["json_data"]["key"] == "newvalue")
+        exdblob = self.assert_get(f"/extension_data/{exdid}")
+        self.assertTrue(exdblob.data["json_data"]["key"] == newvalue)
 
         # Posting with the wrong content_type (user vs host)
         exdblob = self.assert_post_and_400(
             "/extension_data/",
-            {
-                "namespace": nsblob.data["id"],
-                "extension": exblob.data["id"],
-                "content_type": "user",
-                "object_id": hostblob.data["id"],
-                "json_data": {"key": "value", "listkey": [1, 2, 3]},
-            },
+            self._extension_data_blob(extension_id, content_type="user"),
         )
-
-        self.assert_delete("/namespaces/test")
