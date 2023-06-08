64945d66fee15804b30c50258f40db2cf891863d
Terje Kvernes <terje@kvernes.no>
2023-06-09 00:19:21 +0200

    fix(tests): Use raw strings for regex tests

--- hubuum/api/v1/tests/test_40_filters.py
+++ hubuum/api/v1/tests/test_40_filters.py
@@ -168,9 +168,9 @@ class HubuumFilterTestCase(HubuumAPITestCase):
             "/resources/hosts/?name__contains=test&fqdn__contains=domain", 1
         )
         # Regex testing
-        self.assert_get_elements("/resources/hosts/?fqdn__regex=test[23]\.other", 2)
-        self.assert_get_elements("/resources/hosts/?fqdn__regex=test[13].*tld$", 1)
-        self.assert_get_elements("/resources/hosts/?fqdn__regex=^test[13]", 2)
+        self.assert_get_elements(r"/resources/hosts/?fqdn__regex=test[23]\.other", 2)
+        self.assert_get_elements(r"/resources/hosts/?fqdn__regex=test[13].*tld$", 1)
+        self.assert_get_elements(r"/resources/hosts/?fqdn__regex=^test[13]", 2)
 
     def test_extension_data_basic_filtering(self):
         """Test that we can filter into the JSON blobs that extensions deliver."""
